4437aea5092d4636993a74248c67ad9b046fff21
Mira Kabra <mirakabra123@gmail.com>
2023-01-18 11:33:00 -0800

    initial implementation

--- src/include/pfm.h
+++ src/include/pfm.h
@@ -4,7 +4,7 @@
 #define PAGE_SIZE 4096
 
 #include <string>
-
+#include <stdio.h>
 namespace PeterDB {
 
     typedef unsigned PageNum;
@@ -36,7 +36,7 @@ namespace PeterDB {
         unsigned writePageCounter;
         unsigned appendPageCounter;
 
-        FileHandle();                                                       // Default constructor
+        FileHandle();
         ~FileHandle();                                                      // Destructor
 
         RC readPage(PageNum pageNum, void *data);                           // Get a specific page
@@ -45,6 +45,11 @@ namespace PeterDB {
         unsigned getNumberOfPages();                                        // Get the number of pages in the file
         RC collectCounterValues(unsigned &readPageCount, unsigned &writePageCount,
                                 unsigned &appendPageCount);                 // Put current counter values into variables
+        FILE* getFile();
+        void setFile(FILE* file);
+        long int sizeOfFile(FILE* pFile);
+    private:
+        FILE* file;
     };
 
 } // namespace PeterDB
--- src/pfm/pfm.cc
+++ src/pfm/pfm.cc
@@ -1,4 +1,6 @@
 #include "src/include/pfm.h"
+#include <stdio.h>
+#include <cmath>
 
 namespace PeterDB {
     PagedFileManager &PagedFileManager::instance() {
@@ -15,19 +17,51 @@ namespace PeterDB {
     PagedFileManager &PagedFileManager::operator=(const PagedFileManager &) = default;
 
     RC PagedFileManager::createFile(const std::string &fileName) {
-        return -1;
+
+        FILE* pFile;
+
+        pFile = fopen(fileName.c_str(), "a");
+        if(pFile == nullptr){
+            return -1;
+        }
+        fclose(pFile);
+        return 0;
     }
 
     RC PagedFileManager::destroyFile(const std::string &fileName) {
-        return -1;
+        int return_val = remove(fileName.c_str());
+        //if not successful, the return_val wil be nonzero
+        return return_val;
     }
 
     RC PagedFileManager::openFile(const std::string &fileName, FileHandle &fileHandle) {
-        return -1;
+        // If the fileHandle already had a file assigned to it, raise error
+        if(fileHandle.getFile() != nullptr) return -1;
+
+        FILE* pFile;
+        pFile = fopen(fileName.c_str(), "r");
+        // If the file could not be opened, raise error
+        if(pFile == nullptr){
+            return -1;
+        }
+        //Assign the file to fileHandle. File is in Open state now and in read mode
+        fileHandle.setFile(pFile);
+        return 0;
     }
 
     RC PagedFileManager::closeFile(FileHandle &fileHandle) {
-        return -1;
+        //If no file was bound to the fileHandle, raise error
+        if(fileHandle.getFile() == nullptr) return -1;
+
+        //Flush the file data to disk
+        FILE* pFile = fileHandle.getFile();
+        //This should not be needed as it should be flushed when the file is closed
+        fflush(pFile);
+        //close the file
+        fclose(pFile);
+        //After closing the file, set it to null in the fileHandle
+        fileHandle.setFile(nullptr);
+        return 0;
     }
 
     FileHandle::FileHandle() {
@@ -38,11 +72,61 @@ namespace PeterDB {
 
     FileHandle::~FileHandle() = default;
 
+    //Getter function for associated file
+    FILE* FileHandle::getFile(){
+        return file;
+    }
+    //Setter function for associated file
+    void FileHandle::setFile(FILE* file){
+        this->file = file;
+    }
+
     RC FileHandle::readPage(PageNum pageNum, void *data) {
-        return -1;
+        // To access the pages of a file, a client first creates an instance of this class and
+        // passes it to the PagedFileManager::openFile method described above.
+
+        FILE* pFile = this->getFile();
+        //check if the file exists
+        if(pFile == nullptr) return -1;
+
+        //check if the page exists
+        long int size = sizeOfFile(pFile);
+        int totalPagesExcludingHiddenPage = ceil(size / PAGE_SIZE) - 1;
+        //Compare by removing the hidden page
+        if(pageNum+1 > totalPagesExcludingHiddenPage) return -1;
+
+        //The page may not be completely full. In that case,
+        //calculate the actual size of data to be read
+        int size_of_data = 0;
+        if((pageNum + 1)*PAGE_SIZE > size){
+            size_of_data = size - pageNum*PAGE_SIZE;
+        }else{
+            size_of_data = PAGE_SIZE;
+        }
+        //Set position to the start of the pageNum
+        fseek(pFile, pageNum*PAGE_SIZE, SEEK_SET);
+        //read page
+        size_t result = fread(data, size_of_data, 1, pFile);
+        //Increase readPageCount
+        readPageCounter++;
+        return 0;
     }
 
     RC FileHandle::writePage(PageNum pageNum, const void *data) {
+
+        FILE* pFile = this->getFile();
+        //check if the file exists
+        if(pFile == nullptr) return -1;
+
+        //check if the page exists
+        long int size = sizeOfFile(pFile);
+        int totalPagesExcludingHiddenPage = ceil(size / PAGE_SIZE) - 1;
+        //Compare by removing the hidden page
+        if(pageNum+1 > totalPagesExcludingHiddenPage) return -1;
+
+        //The page may have some data. In that case, calculate the position from
+        // which to start
+
         return -1;
     }
 
@@ -56,6 +140,15 @@ namespace PeterDB {
 
     RC FileHandle::collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount) {
         return -1;
+    }
+
+    //Return size of the file
+    long int FileHandle::sizeOfFile(FILE* pFile){
+        fseek(pFile, 0, SEEK_END);
+        long int size = ftell(pFile);
+        //Set position indicator to the beginning of the file
+        rewind(pFile);
+        return size;
     }
 
 } // namespace PeterDB
